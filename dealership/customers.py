c36bbb1618d0faf3dfa822f9881f1d504046d2aa
Lyn Dee <dee.shirlyn@gmail.com>
2017-08-23 21:46:01 +0000

    passed tests

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,16 +1,19 @@
+def is_employee(self):
+    if self.__class__.__name__ is "Employee":
+        return True
+    else:
+        return False
+
 class Person(object):
-    def __init__(self, first_name, last_name, email, is_employee=Null):        
+    def __init__(self, first_name, last_name, email):        
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.is_employee = is_employee
 
 class Customer(Person):
-    def __init__(self, first_name, last_name, email):
-        super(Customer, self).__init__(first_name, last_name, email, is_employee)
-        self.is_employee = False
-
+    def is_employee(self):
+        return False
+        
 class Employee(Person):
-    def __init__(self, first_name, last_name, email):
-	    super(Employee, self).__init__(first_name, last_name, email)
-        self.is_employee = True
+    def is_employee(self):
+        return True
