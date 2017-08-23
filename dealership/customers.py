b4af53ac706f8f6f96af331fb2126e827c64e748
Lyn Dee <yes@lyn.codes>
2017-08-23 17:18:41 -0400

    customers

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,14 +1,16 @@
 class Person(object):
-    def __init__(self, first_name, last_name, email):        
+    def __init__(self, first_name, last_name, email, is_employee=Null):        
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-
+        self.is_employee = is_employee
 
 class Customer(Person):
     def __init__(self, first_name, last_name, email):
-        super(Customer, self).__init__(first_name, last_name, email)
+        super(Customer, self).__init__(first_name, last_name, email, is_employee)
+        self.is_employee = False
 
 class Employee(Person):
     def __init__(self, first_name, last_name, email):
 	    super(Employee, self).__init__(first_name, last_name, email)
+        self.is_employee = True
